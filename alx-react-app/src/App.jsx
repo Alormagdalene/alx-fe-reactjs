865058aa5ecad07d4f65ea667964aeab062e06b4
Magdalene Alor <alormagdalene@gmail.com>
2024-12-05 17:12:35 +0100

    correct component name

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import WelcomeMessage from './component/WelcomeMessage'
-import Header from './component/Header';
-import MainContent from './component/MainContent';
-import Footer from './component/Footer';
-import UserProfile from './component/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
 
