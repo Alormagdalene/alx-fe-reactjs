622dc673d860d6262df9d803310df232b0db4f6d
Magdalene Alor <alormagdalene@gmail.com>
2024-12-10 11:00:42 +0100

    initail commit

---
+++ alx-react-app - new/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header
+        style={{ backgroundColor: "navy", color: "white", textAlign: "centre" }}
+      >
+        <h1>My Favorite Cities</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
